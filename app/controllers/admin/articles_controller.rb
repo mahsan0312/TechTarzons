c979f7b8850703cd4fc21179e6b94bebdb6a9dec
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2024-01-08 23:00:00 +0500

    Articles Addition

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -2,6 +2,5 @@ module Admin
   class ArticlesController < ::ArticlesController
     before_action :authorize_admin!
 
-    # Additional admin-specific actions or overrides can be added here.
   end
 end
