e7358e331915023e04794dfcdcdc5a6deee9bdd4
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2024-01-11 21:43:31 +0500

    Categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_10_182058) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_11_164207) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_article_categories_on_article_id"
+    t.index ["category_id"], name: "index_article_categories_on_category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,6 +27,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_10_182058) do
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles", force: :cascade do |t|
@@ -53,6 +68,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_10_182058) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "article_categories", "articles"
+  add_foreign_key "article_categories", "categories"
   add_foreign_key "articles", "users"
   add_foreign_key "user_roles", "roles"
   add_foreign_key "user_roles", "users"
