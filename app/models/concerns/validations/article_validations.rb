f568580a9308f03420d2869a8a85d9b92c8a0cbf
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2024-01-22 22:14:58 +0500

    New Columns

--- app/models/concerns/validations/article_validations.rb
+++ app/models/concerns/validations/article_validations.rb
@@ -1,4 +1,3 @@
-# app/models/concerns/validations/article_validations.rb
 module Validations
   module ArticleValidations
     extend ActiveSupport::Concern
@@ -6,9 +5,10 @@ module Validations
       validates :title, presence: true, length: { maximum: 255 }
       validates :content, presence: true
       validates :body, presence: true
-      validates :popularity, presence: true
+      validates :popularity, inclusion: { in: Article.popularities.keys }
       validates :paid, inclusion: { in: [true, false] }
       validates :minutes_read, presence: true, numericality: { greater_than_or_equal_to: 0 }
     end
   end
 end
+  
