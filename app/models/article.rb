4f08e529084c1f7e961b9157cd5adc7e20efe7fb
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2024-01-22 22:14:06 +0500

    New Columns

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,9 +6,6 @@ class Article < ApplicationRecord
   has_many :categories, through: :article_categories
 
   enum popularity: { normal: 0, popular: 1 }
-  # Assumes you want to categorize articles as 'normal' or 'popular'
-
-  # Ensures that 'paid' is either true or false
 
   def read_time
     "#{minutes_read} min read"
