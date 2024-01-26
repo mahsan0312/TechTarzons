b6a669c54e86e76741d09ee048f04c641a4e9668
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2024-01-26 23:46:03 +0500

    Improvements

--- app/models/concerns/validations/article_validations.rb
+++ app/models/concerns/validations/article_validations.rb
@@ -3,7 +3,6 @@ module Validations
     extend ActiveSupport::Concern
     included do
       validates :title, presence: true, length: { maximum: 255 }
-      validates :content, presence: true
       validates :body, presence: true
       validates :paid, inclusion: { in: [true, false] }
       validates :minutes_read, presence: true, numericality: { greater_than_or_equal_to: 0 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,37 @@
-# seed.rb
-
-# Clear existing data
 Article.destroy_all
 
+# Find the user
 user = User.find_by(email: "mmahsan0312@gmail.com")
 
-# Create articles with associated users
+# Define articles data
 articles_data = [
   {
     title: "Introduction to Ruby on Rails",
     body: "Ruby on Rails is a powerful web development framework that follows the Model-View-Controller (MVC) pattern. It is known for its convention over configuration and aims to make developers more productive by making assumptions about what every developer needs to get started.",
-    user: user
+    user: user,
+    popularity: :normal
   },
   {
     title: "Getting Started with ReactJS",
     body: "ReactJS is a JavaScript library for building user interfaces. It allows developers to create reusable UI components and manage the state of an application efficiently. React is often used in conjunction with other libraries or frameworks, such as Redux, for state management.",
-    user: user
+    user: user,
+    popularity: :popular
+  },
+  {
+    title: "E-INK DISPLAY COMBINED WITH TACTILE KEYBOARD",
+    body: "Even if it has an E-ink display, the Minimal Phone still pretty much works like its digital siblings since it comes with SIM capability, Wi-Fi, and Bluetooth for calling, texting, and essential email functionality, which are all pre-installed. Apps designed to boost productivity, including a calendar, notes, and basic task management, are also included along with an E-book reader and a simple music player. The Minimal Phone revives the joy of click-clack typing too since it features a tactile, QWERTY keyboard, so the users can feel the buttons they are pressing and hear their clicky sounds, just like in the old days.",
+    user: user,
+    popularity: :normal
+  },
+  {
+    title: "REDSANDS CAMPERS MOBILE VEHICLE, THE WANDERER ",
+    body: "RedSands Campers transforms the Toyota Hilux D4D into a two-person 4WD camper and mobile lodging, complete with a queen-sized bed inside the converted pickup truck and a pull-out breakfast bar and dinner table. Named “The Wanderer,” the truck is also equipped with solar panels to power up devices and electronics in the 4WD camper for travelers. RedSands Campers’ new addition to its growing list of vehicles may be more suitable for exploring Australia all year round, as the Toyota Hilux camper is also equipped with air conditioning to fend off the heat.",
+    user: user,
+    popularity: :popular
   }
 ]
 
+# Create articles
 Article.create(articles_data)
 
 puts 'Seed data has been created.'
