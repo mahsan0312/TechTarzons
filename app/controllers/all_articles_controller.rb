eb173ee25aa42c5e1ede57fb9b7792d7d909727a
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2024-01-26 23:04:38 +0500

    Improvements

--- app/controllers/all_articles_controller.rb
+++ app/controllers/all_articles_controller.rb
@@ -2,8 +2,4 @@ class AllArticlesController < ApplicationController
   def index
     @articles = Article.all # Or fetch articles based on your requirements
   end
-
-  def search
-    # Implement search functionality here
-  end
 end
