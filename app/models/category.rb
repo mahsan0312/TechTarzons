e3c142800aa5a7b2194c30321ea1ed4f73612a78
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2024-02-21 22:42:25 +0500

    new

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
   has_many :article_categories
   has_many :articles, through: :article_categories
+
   validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
 end
